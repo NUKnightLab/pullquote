cdc9cb663df0e74d2e9b2b3f4f8701d2cb275d05
Knight Lab <knightlab@Knights-MBP.lan>
2016-07-15 16:00:29 -0500

    add bookmarklet code

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,28 @@
-/**
-	Pullquote Product Page
-*/ 
+function iFrame() {
+    var iframe, closeButton, backDrop
+
+    function init() {
+        bookMarklet = document.createElement('div');
+        bookMarklet.className = 'bookMarklet';
+        bookMarklet.innerHTML = "<button class='closeButton' onclick='script.iFrame().closeiFrame()'></button><iframe src='/template.html'></iframe><div class='backdrop'></div>"
+
+        document.body.appendChild(bookMarklet);
+
+        var event = new CustomEvent("keydown");
+        window.addEventListener("keydown", function() {
+          event.stopPropagation();
+        }, false)
+
+        window.onkeypress = window.dispatchEvent(event);
+    }
+
+    function closeiFrame() {
+        document.body.removeChild(bookMarklet)
+    }
+
+    return {
+        init: init,
+        closeiFrame: closeiFrame
+    }
+}
 
