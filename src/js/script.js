c3710deb42a26629697f9bb45b98a8d778a18b13
Knight Lab <knightlab@Knights-MBP.lan>
2016-07-15 16:43:20 -0500

    add onClick

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,10 @@ function iFrame() {
   var iframe, closeButton, backDrop
 
   function init() {
+    console.log("hello");
     bookMarklet = document.createElement('div');
     bookMarklet.className = 'bookMarklet';
-    bookMarklet.innerHTML = "<button class='closeButton' onclick='iFrame().closeiFrame()'></button><iframe src='/template.html'></iframe><div class='backdrop'></div>"
+    //bookMarklet.innerHTML = "<button class='closeButton' onclick='iFrame().closeiFrame()'></button><iframe src='compositions.html?quote=whatever quote&image=http://linktoimage&cite=publication name'></iframe><div class='backdrop'></div>"
 
     document.body.appendChild(bookMarklet);
 
