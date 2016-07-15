e149319a28a43f16ad5e8f82c1102a953a251f58
Zach Wise <zach@digitalartwork.net>
2016-07-15 14:49:13 -0500

    Update placeholder to GOT

--- src/js/first.js
+++
@@ -1,9 +0,0 @@
-var second = require('second.js');
-console.log('Color from second is', second.color);
-
-module.exports = {
-  hello: function() {
-    console.log('hello from in here!');       
-  }
-}
-
