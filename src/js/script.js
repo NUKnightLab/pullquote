25e3e7c1c60236dad078275c6da867836d2d697a
Divya Sasidharan <hello@shortdiv.com>
2016-07-15 16:14:10 -0500

    fix: Adding proper click handlers

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@ function iFrame() {
   function init() {
     bookMarklet = document.createElement('div');
     bookMarklet.className = 'bookMarklet';
-    bookMarklet.innerHTML = "<button class='closeButton' onclick='iFrame().closeiFrame()'></button><iframe src='/template.html'></iframe><div class='backdrop'></div>"
+    bookMarklet.innerHTML = "<button class='closeButton' onclick='script.iFrame().closeiFrame()'></button><iframe src='/template.html'></iframe><div class='backdrop'></div>"
 
     document.body.appendChild(bookMarklet);
 
