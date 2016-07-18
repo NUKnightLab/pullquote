3e0f80b4e83c8763c3c847f494bfe7231a838b67
Rebecca Poulson <rebecca.poulson@gmail.com>
2016-07-18 12:16:41 -0500

    rename script to bookmarklet, remove script

--- src/js/bookmarklet.js
+++ src/js/bookmarklet.js
@@ -1,4 +1,37 @@
 /**
 	Pullquote Bookmarklet
 */ 
+function iFrame() {
+
+  function init() {
+      event.preventDefault();
+      bookMarklet = document.createElement('div');
+      bookMarklet.className = 'bookMarklet';
+      bookMarklet.innerHTML = "<button class='closeButton' onClick='bookmarklet.iFrame().closeiFrame()'></button><iframe src='compositions.html?quote=whatever quote&image=assets/placeholder.jpg&cite=publication name'></iframe><div class='backdrop'></div>"
+
+      document.body.appendChild(bookMarklet);
+
+      return false;
+
+      //var event = new CustomEvent("keydown");
+      //window.addEventListener("keydown", function() {
+      //  event.stopPropagation();
+      //}, false)
+
+      //window.onkeypress = window.dispatchEvent(event);
+    }
+
+    function closeiFrame() {
+        document.body.removeChild(bookMarklet)
+    }
+
+    return {
+        init: init,
+        closeiFrame: closeiFrame
+    }
+}
+
+module.exports = {
+    iFrame
+}
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
 var webpack = require('webpack'),
-	path = require('path'),
+    path = require('path'),
 
-	componentPath = path.resolve('./src/js');
+    componentPath = path.resolve('./src/js');
 
 module.exports = {
-	context: path.join(__dirname), 
-	entry: {
-		bookmarklet: "./src/js/bookmarklet.js",
-		overlay: "./src/js/overlay.js",
-		compositions: "./src/js/compositions.js",
-		render: "./src/js/render.js",
-		index: "./src/js/index.js"
-	},
-	output: {
-		path: path.join(__dirname, "./dist/js"),
-		filename: "[name].js"
-	},
-	resolve: {
-		root: componentPath
-	},
-	resolveLoader: {
-		root: path.join(__dirname, "node_modules")
-	}
+    context: path.join(__dirname), 
+    entry: {
+        bookmarklet: "./src/js/bookmarklet.js",
+        overlay: "./src/js/overlay.js",
+        compositions: "./src/js/compositions.js",
+        render: "./src/js/render.js",
+        index: "./src/js/index.js"
+    },
+    output: {
+        path: path.join(__dirname, "./dist/js"),
+        filename: "[name].js"
+    },
+    resolve: {
+        root: componentPath
+    },
+    resolveLoader: {
+        root: path.join(__dirname, "node_modules")
+    }
 }
 
