6c23b7a2d3792123475f033a7a3bd87747477315
Divya Sasidharan <hello@shortdiv.com>
2016-07-18 10:43:26 -0500

    fix: Adding watcher task to ensure files rebuild

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,32 @@
 function iFrame() {
-  var iframe, closeButton, backDrop
+    var iframe, closeButton, backDrop
 
-  function init() {
-    console.log("hello");
-    bookMarklet = document.createElement('div');
-    bookMarklet.className = 'bookMarklet';
-    //bookMarklet.innerHTML = "<button class='closeButton' onclick='iFrame().closeiFrame()'></button><iframe src='compositions.html?quote=whatever quote&image=http://linktoimage&cite=publication name'></iframe><div class='backdrop'></div>"
+    function init() {
+        console.log("hello");
+        bookMarklet = document.createElement('div');
+        bookMarklet.className = 'bookMarklet';
+        bookMarklet.innerHTML = "<button class='closeButton' onclick='script.iFrame().closeiFrame()'></button><iframe src='/template.html'></iframe><div class='backdrop'></div>"
 
-    document.body.appendChild(bookMarklet);
+        document.body.appendChild(bookMarklet);
 
-    var event = new CustomEvent("keydown");
-    window.addEventListener("keydown", function() {
-      event.stopPropagation();
-    }, false)
+        var event = new CustomEvent("keydown");
+        window.addEventListener("keydown", function() {
+          event.stopPropagation();
+        }, false)
 
-    window.onkeypress = window.dispatchEvent(event);
-  }
+        window.onkeypress = window.dispatchEvent(event);
+    }
 
-  function closeiFrame() {
-    document.body.removeChild(bookMarklet)
-  }
+    function closeiFrame() {
+        document.body.removeChild(bookMarklet)
+    }
 
-  return {
-    init: init,
-    closeiFrame: closeiFrame
-  }
+    return {
+        init: init,
+        closeiFrame: closeiFrame
+    }
 }
 
 module.exports = {
-  iFrame
+    iFrame
 }
