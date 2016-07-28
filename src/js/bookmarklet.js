71cb4df14f3cd9682c06b8432b631ace0f2b56c5
Divya Sasidharan <hello@shortdiv.com>
2016-07-28 15:01:48 -0500

    feat: Bookmarklet works as a bookmarklet

--- src/js/bookmarklet.js
+++ src/js/bookmarklet.js
@@ -4,42 +4,51 @@
 function iFrame() {
 
     function init() {
-        event.preventDefault();
         text = grabTextSelection();
         image = grabOgImage();
         url = composeURL(text, image, "publication name")
         bookMarklet = document.createElement('div');
         bookMarklet.className = 'bookMarklet';
-        bookMarklet.innerHTML = "<button class='closeButton' onClick='bookmarklet.iFrame().closeiFrame()'></button><iframe src='" + url + "'></iframe><div class='backdrop'></div>";
+        bookMarklet.innerHTML = "<button class='closeButton' onClick='bookmarklet.iFrame().closeiFrame()'></button><iframe class='pq--iframe' src='" + url + "'></iframe><div class='backdrop'></div>";
 
         document.body.appendChild(bookMarklet);
+        //preventPageScroll();
 
         return false;
     }
 
     function grabTextSelection() {
-      var text = "";
-      text = window.getSelection().toString();
+        var text = "";
+        text = window.getSelection().toString();
 
-      return text;
+        return text;
     }
 
     function grabOgImage() {
-      var image = document.querySelectorAll("meta[property='og:image']")[0].content;
+        if(!!document.querySelectorAll("meta[property='og:image']").length) {
+            var image = document.querySelectorAll("meta[property='og:image']")[0].content;
 
-      return image;
+            return image;
+        } else {
+          return image = "http://placekitten.com/500/300"
+        }
     }
 
     function composeURL(quote, image, cite) {
       //compositions.html?quote=whatever quote&image=assets/placeholder.jpg&cite=publication name
-      var url,
-          q = "quote=" + quote,
-          i = "image=" + image,
-          c = "cite=" + cite;
+        var url,
+            q = "quote=" + quote,
+            i = "image=" + image,
+            c = "cite=" + cite;
 
-      url = "compositions.html?" + q + "&" + i + "&" + c;
+        url = "http://localhost:8080/compositions.html?" + q + "&" + i + "&" + c;
 
-      return url;
+        return url;
+    }
+
+    function preventPageScroll() {
+        var body = document.getElementsByTagName('body');
+        document.body.style.position = "fixed";
     }
 
     function closeiFrame() {
