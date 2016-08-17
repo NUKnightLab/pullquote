42a70c59a7d79572b2e266c0c3e640e7b3b6d233
Divya Sasidharan <hello@shortdiv.com>
2016-08-17 10:55:17 -0500

    fix: Adding handlebars loader and runtime alias to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,13 @@ var webpack = require('webpack'),
 module.exports = {
     context: path.join(__dirname), 
     entry: {
-        bookmarklet: "./src/js/bookmarklet.js",
-        overlay: "./src/js/overlay.js",
-        compositions: "./src/js/compositions.js",
-        render: "./src/js/render.js"
+        vendor: [
+            'handlebars'    
+        ],
+        'bookmarklet': "./src/js/bookmarklet.js",
+        'overlay': "./src/js/overlay.js",
+        'compositions': "./src/js/compositions.js",
+        'render': "./src/js/render.js"
     },
     output: {
         path: path.join(__dirname, "./dist/js"),
@@ -17,25 +20,33 @@ module.exports = {
         libraryTarget: 'var',
         library: "[name]"
     },
+    module: {
+        loaders: [
+            {
+                test: /\.hbs$/,
+                loader: 'handlebars-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin(
+            { name: 'commons', filename: 'common.js', minChunks: 0 }
+        )
+    ],
     node: {
         fs: "empty"
     },
     resolve: {
-        root: componentPath
+        root: componentPath,
+        alias: {
+            'handlebars': 'handlebars/runtime.js'
+        }
     },
     resolveLoader: {
-        root: path.join(__dirname, "node_modules")
-    },
-    module: {
-        loaders: [{
-            test: /\.hbs$/,
-            loader: __dirname + "../../../",
-            query: {
-                partialDirs: [
-                    path.join(__dirname, 'src', 'templates', 'layouts')
-                ]
-            }
-        }]
+        root: path.join(__dirname, "node_modules"),
+        alias: {
+            'hbs': 'handlebars-loader'
+        }
     }
 }
 
